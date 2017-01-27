2d3b4b12268352811c2ad00591354ffe41e5ed4f
Aaron <Aaron@192-168-1-241.tpgi.com.au>
2017-01-27 18:35:31 +1100

    Initial Commit of Dictate.

--- Dictate/ViewController.swift
+++ Dictate/ViewController.swift
@@ -8,22 +8,77 @@
 
 import UIKit
 import TesseractOCR
+import AVFoundation
 
-var language = "eng"
+var language = "vi-VN"
 
 class ViewController: UIViewController, G8TesseractDelegate {
 
+    @IBOutlet weak var cameraView: UIView!
+    
+    @IBOutlet weak var pause: UIButton!
+    @IBAction func stopMotion(_ sender: Any) {
+        print("pressed")
+        pause.tintColor = UIColor.lightGray
+    }
+    @IBAction func revertBTNColour(_ sender: Any) {
+        pause.tintColor = UIColor.black
+    }
+    
     let selectedLanguage = { () -> String in 
         //if language == "eng" {
             return language
         //}
     }()
     
+    var captureSession = AVCaptureSession()
+    var sessionOutput = AVCaptureStillImageOutput()
+    var previewLayer = AVCaptureVideoPreviewLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let tesseract = G8Tesseract(language: "eng") {
+        let devices = AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo)
+        for device in devices! {
+            if (device as AnyObject).position == AVCaptureDevicePosition.back {
+                
+                do {
+                    
+                    let input = try AVCaptureDeviceInput(device: device as! AVCaptureDevice)
+                    if captureSession.canAddInput(input) {
+                        
+                        captureSession.addInput(input)
+                        sessionOutput.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+                        
+                        if captureSession.canAddOutput(sessionOutput) {
+                            
+                            captureSession.addOutput(sessionOutput)
+                            captureSession.startRunning()
+                            
+                            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                            previewLayer.connection.videoOrientation = AVCaptureVideoOrientation.portrait
+                            
+                            cameraView.layer.addSublayer(previewLayer)
+                            
+                            previewLayer.position = CGPoint(x: self.cameraView.frame.width / 2, y: self.cameraView.frame.height / 2)
+                            previewLayer.bounds = cameraView.frame
+                            
+                        }
+                        
+                    }
+                    
+                } catch {
+                    print(error.localizedDescription)
+                }
+                
+            } else {
+                print("No device")
+            }
+        }
+        
+        if let tesseract = G8Tesseract(language: "\(selectedLanguage)") {
             tesseract.delegate = self
             tesseract.image = UIImage(named: "testPoem")?.g8_blackAndWhite()
             tesseract.recognize()
@@ -32,6 +87,8 @@ class ViewController: UIViewController, G8TesseractDelegate {
             
             print(recognizedText!)
         }
+        
+        pause.tintColor = UIColor.black
         
     }
 
@@ -42,6 +99,13 @@ class ViewController: UIViewController, G8TesseractDelegate {
 
     func progressImageRecognition(for tesseract: G8Tesseract!) {
         print("Overall process: \(tesseract.progress)%")
+        if tesseract.progress == 100 {
+            let synth = AVSpeechSynthesizer()
+            let utterance = AVSpeechUtterance(string: "\(tesseract.recognizedText)")
+            utterance.rate = 0.5
+            utterance.voice = AVSpeechSynthesisVoice(language: selectedLanguage)
+            synth.speak(utterance)
+        }
     }
 
 }
